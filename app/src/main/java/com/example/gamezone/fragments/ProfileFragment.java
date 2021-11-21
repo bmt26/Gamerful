4a4035fefa1bb8d9326433a24dbfaa0fef30ba78
ahirpara2000 <ahirpara2000@gmail.com>
2021-11-21 11:24:05 -0500

    Added logout button to profile page

--- app/src/main/java/com/example/gamezone/fragments/ProfileFragment.java
+++ app/src/main/java/com/example/gamezone/fragments/ProfileFragment.java
@@ -1,20 +1,28 @@
 package com.example.gamezone.fragments;
 
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.gamezone.LoginActivity;
 import com.example.gamezone.R;
+import com.parse.ParseUser;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class ProfileFragment extends Fragment {
+
+    private Button btnLogout;
 
 
     public ProfileFragment() {
@@ -29,4 +37,20 @@ public class ProfileFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        btnLogout = view.findViewById(R.id.btnLogout);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ParseUser.logOut();
+                ParseUser currentUser = ParseUser.getCurrentUser(); // this will now be null
+
+                Intent i = new Intent(getContext(), LoginActivity.class);
+                startActivity(i);
+                getActivity().finish();
+            }
+        });
+    }
 }
