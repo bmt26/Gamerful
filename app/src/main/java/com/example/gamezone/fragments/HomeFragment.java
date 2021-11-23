f271be9dfba5713a76abe9ffbfb5fe1a9014333e
ahirpara2000 <ahirpara2000@gmail.com>
2021-11-23 17:49:42 -0500

    Added secrets gradle plugin to store api key in local.properties

--- app/src/main/java/com/example/gamezone/fragments/HomeFragment.java
+++ app/src/main/java/com/example/gamezone/fragments/HomeFragment.java
@@ -3,12 +3,16 @@ package com.example.gamezone.fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.gamezone.BuildConfig;
 import com.example.gamezone.R;
 
 /**
