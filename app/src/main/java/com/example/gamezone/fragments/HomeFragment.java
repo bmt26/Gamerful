d1409bf67de53382a72b13f8d4a119668c288dac
ahirpara2000 <ahirpara2000@gmail.com>
2021-11-27 17:03:33 -0500

    added upcoming games model

--- app/src/main/java/com/example/gamezone/fragments/HomeFragment.java
+++ app/src/main/java/com/example/gamezone/fragments/HomeFragment.java
@@ -35,8 +35,10 @@ public class HomeFragment extends Fragment {
     public static final String TAG = "HomeFragment";
     public static final String API_KEY = BuildConfig.RAWG_KEY;
     public static final String RECENT_GAME = "https://rawg.io/api/games/lists/recent-games";
+    public static final String UPCOMING_GAMES = "https://rawg.io/api/games/lists/recent-games-future";
 
     List<Games> topGames;
+    List<Games> upcomingGames;
 
     public HomeFragment() {
         // Required empty public constructor
@@ -55,6 +57,8 @@ public class HomeFragment extends Fragment {
 
         AsyncHttpClient client = new AsyncHttpClient();
 
+        // Top Games this week
+
         RequestParams params = new RequestParams();
         params.put("discover", true);
         params.put("ordering", "-relevance");
@@ -70,6 +74,37 @@ public class HomeFragment extends Fragment {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(TAG, "Results: " + results);
                     topGames = Games.fromJsonArray(results);
+                    Log.d(TAG, "Top games: " + topGames.get(0).getName());
+                } catch (JSONException e) {
+                    Log.e(TAG, "Hit JSON exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.d(TAG, "onFailure");
+            }
+        });
+
+        // Upcoming games
+
+        params = new RequestParams();
+        params.put("discover", true);
+        params.put("ordering", "-added");
+        params.put("page", 1);
+        params.put("page_size", 10);
+        params.put("key", API_KEY);
+        client.get(UPCOMING_GAMES, params, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(TAG, "Results: " + results);
+                    upcomingGames = Games.fromJsonArray(results);
+                    Log.d(TAG, "Upcoming games: " + upcomingGames.get(0).getName());
                 } catch (JSONException e) {
                     Log.e(TAG, "Hit JSON exception", e);
                     e.printStackTrace();
